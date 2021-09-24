f60d861039d6a2826d01bd6f4b65a55d1032d7d4
gravitacia <71985277+gravitacia@users.noreply.github.com>
2021-09-24 12:22:49 +0300

    feat

---
+++ Isu/Modules/CourseNumber.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Isu.Modules
+{
+    public class CourseNumber
+    {
+        public CourseNumber(char number)
+        {
+            Number = number;
+        }
+
+        public char Number { get; }
+    }
+}
---
+++ Isu/Modules/Student.cs
@@ -0,0 +1,16 @@
+﻿namespace Isu.Modules
+{
+    public class Student
+    {
+        public Student(string name, int id, string group_name)
+        {
+            Name = name;
+            Id = id;
+            GroupName = group_name;
+        }
+
+        public string Name { get; }
+        public int Id { get; }
+        public string GroupName { get; set; }
+    }
+}
