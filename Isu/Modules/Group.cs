366f1de4aa9df8bec8001f178c0714f20fa3d233
gravitacia <71985277+gravitacia@users.noreply.github.com>
2021-09-24 12:22:49 +0300

    feat : new build

---
+++ Isu/Modules/Group.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Isu.Tools;
+
+namespace Isu.Modules
+{
+    public class Group
+    {
+        public Group(string name)
+        {
+            GroupName = name;
+            StudentsList = new List<Student>();
+            AllStudents = new List<Student>();
+
+            if (!IsAllowed(name)) throw new IsuException("This group got wrong name");
+        }
+
+        public List<Student> StudentsList { get; set; }
+        public List<Student> AllStudents { get; set; }
+
+        public string GroupName { get; }
+        private bool IsAllowed(string name)
+        {
+            return name.Length == 5 && name[2] < '5' && name[2] > '0';
+        }
+    }
+}
---
+++ Isu/Services/IsuService.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Isu.Modules;
+using Isu.Tools;
+
+namespace Isu.Services
+{
+    public class IsuService : IIsuService
+    {
+       private List<Group> _groups = new List<Group>();
+       private int _studentsCounter = 0;
+       private int _studentsInGroup = 0;
+
+       public Group AddGroup(string name)
+       {
+           var group = new Group(name);
+           _groups.Add(group);
+           return group;
+       }
+
+       public Student AddStudent(Group group, string name)
+       {
+           var student = new Student(name, _studentsCounter++, group.GroupName);
+           if (_studentsInGroup < 20)
+           {
+               foreach (Group curGroup in _groups.Where(curGroup => curGroup == @group))
+               {
+                   curGroup.StudentsList.Add(student);
+               }
+
+               _studentsInGroup++;
+           }
+           else
+           {
+               throw new IsuException("Too many students");
+           }
+
+           return student;
+       }
+
+       public Student GetStudent(int id)
+       {
+           if (id > _studentsCounter)
+           {
+               throw new Exception("WARNING! Student not found.");
+           }
+           else
+           {
+               return _groups.SelectMany(curGroup => curGroup.StudentsList).FirstOrDefault(curStudent => curStudent.Id == id);
+           }
+       }
+
+       public Student FindStudent(string name)
+       {
+           return _groups.SelectMany(curGroup => curGroup.StudentsList).FirstOrDefault(curStudent => curStudent.Name == name);
+       }
+
+       public List<Student> FindStudents(string groupName)
+       {
+           return _groups.Where(curGroup => curGroup.GroupName == groupName).Select(curGroup => curGroup.StudentsList).FirstOrDefault();
+       }
+
+       public List<Student> FindStudents(CourseNumber courseNumber)
+       {
+           var allStudents = new List<Student>();
+           foreach (Group curGroup in _groups.Where(curGroup => curGroup.GroupName[2] == courseNumber.Number))
+           {
+               allStudents.AddRange(curGroup.StudentsList);
+           }
+
+           return allStudents;
+       }
+
+       public Group FindGroup(string groupName)
+       {
+           return _groups.FirstOrDefault(curGroup => curGroup.GroupName == groupName);
+       }
+
+       public List<Group> FindGroups(CourseNumber courseNumber)
+       {
+           return _groups.Where(curGroup => curGroup.GroupName[2] == courseNumber.Number).ToList();
+       }
+
+       public void ChangeStudentGroup(Student student, Group newGroup)
+       {
+           foreach (Group curGroup in _groups)
+           {
+               curGroup.StudentsList.Remove(student);
+               AddStudent(newGroup, student.Name);
+           }
+       }
+    }
+}
