323ca8514e6778b700ff748d6ad9fc6704675d45
gravitacia <kocagg@mail.ru>
2021-12-05 22:06:53 +0300

    feat : some fixes

--- Banks/User/User.cs
+++ Banks/User/User.cs
@@ -1,30 +1,9 @@
 using System.Collections.Generic;
+using Banks.Bank;
 
 namespace Banks.User
 {
     public class User
     {
-        private string _id;
-        private string _name;
-        private string _surname;
-        private string _address;
-        private string _passport;
-
-        public User(string name, string surname, string address, string passport, BankInformation bankInfo)
-        {
-            _id = bankInfo.AvailibleClientId.ToString();
-            _name = name;
-            _surname = surname;
-            _address = address;
-            _passport = passport;
-        }
-
-        public bool IsTrustful()
-        {
-            if (_address == string.Empty || _passport == string.Empty)
-                return false;
-            else
-                return true;
-        }
     }
 }
